b35d473ba56c92932523a9d44c4e3141095fc809
Hari <hksshah111@gmail.com>
2023-08-08 18:23:02 +0545

    Second Commit

--- sunday.js
+++ sunday.js
@@ -1,22 +1,23 @@
-// alert("I am Hari");
-// let x = prompt("Enter the first number");
-// let y = prompt("Enter the first number");
+// // alert("I am Hari");
+// // let x = prompt("Enter the first number");
+// // let y = prompt("Enter the first number");
 
-// let ans = x + y;
+// // let ans = x + y;
 
-// x>y?document.getElementById("answer").innerHTML += "X is greater":document.getElementById("answer").innerHTML += "y  is greater";
-// document.getElementById("answer").innerHTML += "The sum is " + ans;
+// // x>y?document.getElementById("answer").innerHTML += "X is greater":document.getElementById("answer").innerHTML += "y  is greater";
+// // document.getElementById("answer").innerHTML += "The sum is " + ans;
 
-let gender = prompt("Type male if you are mail else type female");
+// let gender = prompt("Type male if you are mail else type female");
 
-if(gender === "male")
-{
-    let salary = Number(prompt("Enter your salary"));
-    salary>25000?alert("Salary is greater than " + 25000): alert("Salary is lesser than " + 25000);
-}
+// if(gender === "male")
+// {
+//     let salary = Number(prompt("Enter your salary"));
+//     salary>25000?alert("Salary is greater than " + 25000): alert("Salary is lesser than " + 25000);
+// }
 
-else if(gender === "female")
-{
-    let age = Number(prompt("Enter your age"));
-    age>20?alert("You are legally allowed to marry"):alert("You are not legally allowed to marry");
-}
+// else if(gender === "female")
+// {
+//     let age = Number(prompt("Enter your age"));
+//     age>20?alert("You are legally allowed to marry"):alert("You are not legally allowed to marry");
+// }
+
