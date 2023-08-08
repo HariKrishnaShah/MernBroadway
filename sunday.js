a51386966dbd96642f25d6932c8b87191e3efe30
Hari <hksshah111@gmail.com>
2023-08-08 18:58:10 +0545

    Third Day

--- sunday.js
+++ sunday.js
@@ -21,3 +21,40 @@
 //     age>20?alert("You are legally allowed to marry"):alert("You are not legally allowed to marry");
 // }
 
+// let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+// let days = "The days of the weeks are "
+
+// for(let i = 0;  i<week.length; i++)
+// {
+//     days += week[i] + " ";
+// }
+// alert(days);
+
+const x = Number(prompt("Enter the first number"));
+const y = Number(prompt("Enter the Second number"));
+let ans = 0;
+const action = Number(prompt("Enter 1 to add, 2 to subtract, 3 to multiply and 4 to divide"));
+
+switch(action)
+{
+    case 1:
+        ans = x + y;
+        alert("The answer is " + ans);
+        break;
+    case 2:
+        ans = x - y;
+        alert("The answer is " + ans);
+        break;
+    case 3:
+        ans = x * y;
+        alert("The answer is " + ans);
+        break;
+    case 4:
+        ans = x / y;
+        alert("The answer is " + ans);
+        break;
+    default:
+        alert("Invalid command")
+
+}
+
