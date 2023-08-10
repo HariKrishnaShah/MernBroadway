833188d34067af718bdb6bbc7e594c6d5850b622
Hari <hksshah111@gmail.com>
2023-08-10 15:44:19 +0545

    array methods

--- sunday.js
+++ sunday.js
@@ -1,74 +1,117 @@
-// // alert("I am Hari");
-// // let x = prompt("Enter the first number");
-// // let y = prompt("Enter the first number");
+// // // alert("I am Hari");
+// // // let x = prompt("Enter the first number");
+// // // let y = prompt("Enter the first number");
 
-// // let ans = x + y;
+// // // let ans = x + y;
 
-// // x>y?document.getElementById("answer").innerHTML += "X is greater":document.getElementById("answer").innerHTML += "y  is greater";
-// // document.getElementById("answer").innerHTML += "The sum is " + ans;
+// // // x>y?document.getElementById("answer").innerHTML += "X is greater":document.getElementById("answer").innerHTML += "y  is greater";
+// // // document.getElementById("answer").innerHTML += "The sum is " + ans;
 
-// let gender = prompt("Type male if you are mail else type female");
+// // let gender = prompt("Type male if you are mail else type female");
 
-// if(gender === "male")
-// {
-//     let salary = Number(prompt("Enter your salary"));
-//     salary>25000?alert("Salary is greater than " + 25000): alert("Salary is lesser than " + 25000);
-// }
+// // if(gender === "male")
+// // {
+// //     let salary = Number(prompt("Enter your salary"));
+// //     salary>25000?alert("Salary is greater than " + 25000): alert("Salary is lesser than " + 25000);
+// // }
 
-// else if(gender === "female")
-// {
-//     let age = Number(prompt("Enter your age"));
-//     age>20?alert("You are legally allowed to marry"):alert("You are not legally allowed to marry");
-// }
+// // else if(gender === "female")
+// // {
+// //     let age = Number(prompt("Enter your age"));
+// //     age>20?alert("You are legally allowed to marry"):alert("You are not legally allowed to marry");
+// // }
 
-// let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-// let days = "The days of the weeks are "
+// // let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+// // let days = "The days of the weeks are "
 
-// for(let i = 0;  i<week.length; i++)
-// {
-//     days += week[i] + " ";
-// }
-// alert(days);
+// // for(let i = 0;  i<week.length; i++)
+// // {
+// //     days += week[i] + " ";
+// // }
+// // alert(days);
+
+// // const x = Number(prompt("Enter the first number"));
+// // const y = Number(prompt("Enter the Second number"));
+// // let ans = 0;
+// // const action = Number(prompt("Enter 1 to add, 2 to subtract, 3 to multiply and 4 to divide"));
+
+// // switch(action)
+// // {
+// //     case 1:
+// //         ans = x + y;
+// //         alert("The answer is " + ans);
+// //         break;
+// //     case 2:
+// //         ans = x - y;
+// //         alert("The answer is " + ans);
+// //         break;
+// //     case 3:
+// //         ans = x * y;
+// //         alert("The answer is " + ans);
+// //         break;
+// //     case 4:
+// //         ans = x / y;
+// //         alert("The answer is " + ans);
+// //         break;
+// //     default:
+// //         alert("Invalid command")
+// // }
+
+// // const returnUpper = (name) =>{return (name.toUpperCase())}
+// // alert("The name in uppercase is  " + returnUpper("hari shah"));
 
 // const x = Number(prompt("Enter the first number"));
-// const y = Number(prompt("Enter the Second number"));
-// let ans = 0;
-// const action = Number(prompt("Enter 1 to add, 2 to subtract, 3 to multiply and 4 to divide"));
+// const y = Number(prompt("Enter the second number"));
+// const add = (a, b) =>{return a+b};
+// let ans = add(x, y);
+// alert("The sum is " + ans);
 
-// switch(action)
+
+// const mult = (a, b) =>{return a*b};
+// ans = mult(x, y);
+// alert("The multiplication is " + ans);
+
+
+// const person = {
+//     fname: "Hari",
+//     lname:"Shah",
+//     fullname:(fname, lname)=>
+//     {
+//         return fname + " " + lname;
+//     }
+// }
+// console.log(person.fullname())
+
+
+let countries = ["United States of America", "Nepal", "Bangladesh"]
+
+// function findlongest()
 // {
-//     case 1:
-//         ans = x + y;
-//         alert("The answer is " + ans);
-//         break;
-//     case 2:
-//         ans = x - y;
-//         alert("The answer is " + ans);
-//         break;
-//     case 3:
-//         ans = x * y;
-//         alert("The answer is " + ans);
-//         break;
-//     case 4:
-//         ans = x / y;
-//         alert("The answer is " + ans);
-//         break;
-//     default:
-//         alert("Invalid command")
+//     let  index = 0;
+//     let length = 0;
+//     for(let i = 0; i<countries.length; i++)
+//     {
+//         if(countries[i].length >= length)
+//         {
+//             length = countries[i].length;
+//             index = i;
+//         }
+//     }
+//     alert("The longest name is " + countries[index] + " with length " + length)
 // }
 
-// const returnUpper = (name) =>{return (name.toUpperCase())}
-// alert("The name in uppercase is  " + returnUpper("hari shah"));
+// findlongest();
 
-const x = Number(prompt("Enter the first number"));
-const y = Number(prompt("Enter the second number"));
-const add = (a, b) =>{return a+b};
-let ans = add(x, y);
-alert("The sum is " + ans);
+let index  = 0;
+let l = 0;
+let longestcountry = countries.map((a)=>{
+    if(a.length >= l)
+    {
+        l = a.length;
+        return a;
+       
+    }
+})
 
-
-const mult = (a, b) =>{return a*b};
-ans = mult(x, y);
-alert("The multiplication is " + ans);
-
+alert("The longest is " + longestcountry);
 
